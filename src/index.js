ecb1781125d05055618ddf13743e986f76a5b8d4
Xavier Castaner <xavier.castaner@moodys.com>
2022-03-03 11:34:33 +0100

    Change build command

--- src/index.js
+++
@@ -1,3 +0,0 @@
-export { default as HelloWorld } from "./hello-world";
-export { default as Hello } from "./hello";
-export { default as World } from "./world";
---
+++ src/lib/index.js
@@ -0,0 +1,5 @@
+import HelloWorld from "./hello-world";
+import Hello from "./hello";
+import World from "./world";
+
+export { HelloWorld, Hello, World };
